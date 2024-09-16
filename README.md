7585bcba7db51c14a77197e03c8d8796be60addb
BeboKhouja <73379481+BeboKhouja@users.noreply.github.com>
2024-09-16 09:33:51 +0700

    Add links

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 The website for PC Simulator.
 
 # Links
-If you are on Android, install this app for better performance: https://github.com/BeboKhouja/PCSimulatorSaveEditorAndroidPort/releases/download/v1.4.3/app-release.apk<br>
-If you are on PC, or a platform where a JVM is available, install this program for better performance: https://github.com/BeboKhouja/PC-Simulator-Save-Editor/releases/tag/v1.3.0<br>
-If you want to host a server, install this program: https://github.com/BeboKhouja/PC-Simulator-Save-Editor-Server
+If you are on Android, install this app for better performance (supports all architectures supported by Android): https://github.com/BeboKhouja/PCSimulatorSaveEditorAndroidPort/releases/download/v1.4.3/app-release.apk<br>
+If you are on Windows 10 or later, install this program for better performance (x86, x86-64, AArch64): https://github.com/BeboKhouja/PC-Simulator-Save-Editor-for-Windows-10<br>
+If you are on Windows 8.1 or earlier, Mac, Linux, or a platform where a JVM is available, install this program for better performance (supports all architectures supported by Java): https://github.com/BeboKhouja/PC-Simulator-Save-Editor/releases/tag/v1.3.0<br>
+If you want to host a server, install this program (supports all architectures supported by Java): https://github.com/BeboKhouja/PC-Simulator-Save-Editor-Server<br>
+Otherwise, use this website (all browsers with JavaScript enabled).
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+/*
+PC Simulator Save Editor is a free and open source save editor for PC Simulator.
+    Copyright (C) 2024  Mokka Chocolata
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+Email: mokkachocolata@gmail.com
+*/
+
 // First time writing JS
 window.onload=function() {
-    var textarea = document.getElementById("textarea")
-    var clicked = false
+    var textarea = document.getElementById("textarea");
+    var clicked = false;
     function copyToClipboard() {
-        navigator.clipboard.writeText(textarea.value)
+        navigator.clipboard.writeText(textarea.value);
     }
     function readSingleFile(e) {
         var file = e.target.files[0];
@@ -13,12 +32,12 @@ window.onload=function() {
         var reader = new FileReader();
         reader.onload = function(e) {
           var contents = e.target.result;
-          textarea.value = decryptStr(contents)
-          clicked = false
+          textarea.value = decryptStr(contents);
+          clicked = false;
           setTimeout(function(){
-                clicked = true
-          }, 10)
-        };
+                clicked = true;
+          }, 10);
+        }
         reader.readAsText(file);
     }
     function download(filename, text) {
@@ -35,23 +54,26 @@ window.onload=function() {
             pom.click();
         }
     }
+    // Decrypt provided string
     function decryptStr(str) {
-        var key = 0x81
-        var out = ""
+        var key = 0x81;
+        var out = "";
         for (let i = 0; i < str.length; i++) {
-            out += String.fromCharCode(str.charCodeAt(i) ^ key)
+            out += String.fromCharCode(str.charCodeAt(i) ^ key);
         }
-        return out
+        return out;
     }
+    // Just basically a shortcut to the decryptStr function
     function decrypt() {
-        textarea.value = decryptStr(textarea.value)
+        textarea.value = decryptStr(textarea.value);
     }
     function down() {
-        download("Save.pc", decryptStr(textarea.value))
+        download("Save.pc", decryptStr(textarea.value));
     }
-    var open = document.getElementById("file-input")
-    open.addEventListener("click", readSingleFile, false)
-    var openDecrypt = document.getElementById("file-decrypttotxt")
+    var open = document.getElementById("file-input");
+    open.addEventListener("click", readSingleFile, false);
+    var openDecrypt = document.getElementById("file-decrypttotxt");
+    // Decrypts the opened file, then saves it as a decrypted file
     function decryptToTxt(e) {
         var file = e.target.files[0];
         if (!file) {
@@ -60,23 +82,23 @@ window.onload=function() {
         var reader = new FileReader();
         reader.onload = function(e) {
           var contents = e.target.result;
-          download("Save.pc", decryptStr(contents))
+          download("Save.pc", decryptStr(contents));
         };
         reader.readAsText(file);
     }
-    openDecrypt.addEventListener("click", decryptToTxt, false)
-    var button = document.getElementById("decrypt/encrypt")
-    button.addEventListener("click", decrypt, false)
-    var copy = document.getElementById("copy")
-    copy.addEventListener("click", copyToClipboard, false)
-    var openFile = document.getElementById("open")
+    openDecrypt.addEventListener("click", decryptToTxt, false);
+    var button = document.getElementById("decrypt/encrypt");
+    button.addEventListener("click", decrypt, false);
+    var copy = document.getElementById("copy");
+    copy.addEventListener("click", copyToClipboard, false);
+    var openFile = document.getElementById("open");
     openFile.addEventListener("click", function() {
-        open.click()
-    }, false)
-    var save = document.getElementById("save")
-    save.addEventListener("click", down, false)
-    var decrypttotxt = document.getElementById("decrypttotxt")
+        open.click();
+    }, false);
+    var save = document.getElementById("save");
+    save.addEventListener("click", down, false);
+    var decrypttotxt = document.getElementById("decrypttotxt");
     decrypttotxt.addEventListener("click", function() {
-        openDecrypt.click()
-    }, false)
+        openDecrypt.click();
+    }, false);
 }
